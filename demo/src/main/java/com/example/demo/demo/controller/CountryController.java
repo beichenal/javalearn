07fe16ac92d63885fb83e06e612a36a1bd877d0d
dengliangdong <1615527807@qq.com>
2023-04-15 09:34:27 +0800

    add sql

--- demo/src/main/java/com/example/demo/demo/controller/CountryController.java
+++ demo/src/main/java/com/example/demo/demo/controller/CountryController.java
@@ -10,15 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.demo.entity.Country;
 import com.example.demo.demo.mapper.CountryMapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 @RestController
 @RequestMapping("/api")
 public class CountryController {
+
+  private static final Logger log = LoggerFactory.getLogger(CountryController.class);
 
   @Autowired
   CountryMapper countryMapper;
 
   @GetMapping("/countries")
   public List<Country> getAllCountry() {
+    log.info("查询开始");
     return countryMapper.selectAll();
   }
 }
--- sql/rbac.sql
+++
@@ -1,3 +0,0 @@
-create database if not exists mybatis default character set utf8 collate utf8_gengeral_ci;
-
-use mybatis;
